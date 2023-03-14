e95e3b54937b7c5d6d6e3e6a8e419bfba675ea89
ios <wow5702@naver.com>
2023-03-14 17:55:32 +0900

    테스트 이미지 제거 & label tap_gesture 구현

--- ABC/GameMainViewController.swift
+++ ABC/GameMainViewController.swift
@@ -7,22 +7,31 @@
 
 import UIKit
 
-var images = ["01.png", "02.png"]
 class GameMainViewController: UIViewController {
 
-    @IBOutlet var imgReaction: UIImageView!
     @IBOutlet var test: UILabel!
     @IBOutlet var test2: UILabel!
     @IBOutlet var mili: UILabel!
+    @IBOutlet var touchlabel: UILabel!
     
     var checkTime = CFAbsoluteTimeGetCurrent()
     
     override func viewDidLoad() {
-        imgReaction.image = UIImage(named : images[0])
         super.viewDidLoad()
 
-
+        touchlabel.text = "반응속도 테스트 입니다. \n준비가 된다면 레이블을 눌러 시작하십시오. \n테스트는 연속해서 5회 반복됩니다."
+        self.touchlabel.backgroundColor = UIColor.gray
         // Do any additional setup after loading the view.
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeLabel))
+        touchlabel.addGestureRecognizer(tapGestureRecognizer)
+        
+    }
+    @objc func changeLabel() {
+        var count = 1
+        self.touchlabel.text = "\(count)번째 측정을 시작합니다.\n화면이 "
+        self.touchlabel.backgroundColor = UIColor.green
+        
     }
     
     // 터치가 시작될때 호출
@@ -31,7 +40,6 @@ class GameMainViewController: UIViewController {
         
         test.text = "Touches Began"
         mili.text = "측정 시작"
-        imgReaction.image = UIImage(named: images[1])
         
         checkTime = CFAbsoluteTimeGetCurrent()
         
@@ -44,7 +52,6 @@ class GameMainViewController: UIViewController {
         test.text = "Touches Ended"
         var changemilli = (CFAbsoluteTimeGetCurrent() - checkTime)*1000
         mili.text = "소요 시간 : \(Int(changemilli)) ms 입니다."
-        imgReaction.image = UIImage(named: images[0])
     }
     
 }
