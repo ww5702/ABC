bec8f187d0ac65ec1b9618dea6c9ff7fea2f7a47
ios <wow5702@naver.com>
2023-03-16 16:56:23 +0900

    mili label -> table로 교체 & 지정횟수 끝날시 문구 출력

--- ABC/GameMainViewController.swift
+++ ABC/GameMainViewController.swift
@@ -7,17 +7,22 @@
 
 import UIKit
 
-class GameMainViewController: UIViewController {
-
-    @IBOutlet var mili: UILabel!
+class GameMainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet var countlabel: UILabel!
     @IBOutlet var touchlabel: UILabel!
     
     var checkTime = CFAbsoluteTimeGetCurrent()
-    var checkEnd = true
     var isGameStart = false
+    var isColorChange = false
     var count = 1
     var color = "gray"
+    var reactionarr : [Int] = [1,2,3,4,5]
+    
+    // time trigger
+    var timeTrigger = true
+    var realTime = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,37 +34,60 @@ class GameMainViewController: UIViewController {
         /*
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeLabel))
         touchlabel.addGestureRecognizer(tapGestureRecognizer)*/
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let text:String = String(reactionarr[indexPath.row])
+        cell.textLabel?.text = text
+        return cell
     }
     
     
     @IBAction func btnGameStart(_ sender: UIButton) {
-        self.countlabel.text = "\(count)번째 측정"
-        self.touchlabel.text = "빨간 화면이 초록색 화면으로\n변한다면 눌러주세요."
-        self.touchlabel.backgroundColor = UIColor.red
         if isGameStart == true {
             let alert = UIAlertController(title: "경고", message: "이미 게임이 진행중입니다.", preferredStyle: UIAlertController.Style.alert)
             let check = UIAlertAction(title: "확인", style: .default, handler: nil)
             alert.addAction(check)
-        } else {
-            waitend()
+        }
+        self.countlabel.text = "준비중입니다."
+        self.touchlabel.text = "빨간 화면이 초록색 화면으로\n변한다면 눌러주세요."
+        self.touchlabel.backgroundColor = UIColor.red
+        while(timeTrigger) {
+            checkTimeTrigger()
         }
     }
+    func checkTimeTrigger() {
+        realTime = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(changecolor), userInfo: nil, repeats: true)
+        timeTrigger = false
+    }
+    func stopTimer() {
+        timeTrigger = true
+        realTime.invalidate()
+        isGameStart = false
+    }
     
-    func changecolor() {
+    @objc func changecolor() {
         self.isGameStart = true
-        self.checkEnd = false
         let randomTime = Double.random(in: 3.0...8.0)
+        self.countlabel.text = "\(count)번째 측정"
+        print("count : \(count)")
         print("randomTime : \(randomTime)")
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+randomTime) {
             self.touchlabel.backgroundColor = UIColor.green
-            self.color = "green"
+            self.isColorChange = true
             print("함수 실행")
+            if self.count >= 2 {
+                self.stopTimer()
+            }
         }
     }
-    
-    func waitend() {
-        changecolor()
-    }
+
     /*
     @objc func changeLabel() {
         var color = "gray"
@@ -88,7 +116,7 @@ class GameMainViewController: UIViewController {
     // 터치가 시작될때 호출
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //let touch = touches.first! as UITouch
-        if self.color == "green" {
+        if self.isColorChange == true {
             self.checkTime = CFAbsoluteTimeGetCurrent()
         }
     }
@@ -96,15 +124,19 @@ class GameMainViewController: UIViewController {
     // 화면에서 손가락을 떼었을 떄 호출
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         //let touch = touches.first! as UITouch
-        if self.color == "green" {
+        if self.isColorChange == true {
             let changemilli = (CFAbsoluteTimeGetCurrent() - self.checkTime)*1000
-            self.mili.text = "소요 시간 : \(Int(changemilli)) ms 입니다."
+            reactionarr[count-1] = Int(changemilli)
             
             // 다시 빨간색으로 변경
             self.touchlabel.backgroundColor = UIColor.red
-            self.color = "red"
-            self.checkEnd = true
+            self.isColorChange = false
             count += 1
+            
+            if count > 2 {
+                self.touchlabel.text = "측정이 전부 끝났습니다!!"
+                print(reactionarr)
+            }
         }
         
     }
