408ca3aa718158e7c5864821bfe17a7a6deba365
ios <wow5702@naver.com>
2023-03-27 17:32:51 +0900

    하나의 뷰에서 여러 뷰 컨트롤러 사용

--- ABC/VisualMemoryViewController.swift
+++ ABC/VisualMemoryViewController.swift
@@ -8,16 +8,26 @@
 import UIKit
 
 class VisualMemoryViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        firstView.alpha = 1
+        secondView.alpha = 0
+    }
+
+    @IBAction func asdf(_ sender: Any) {
+        firstView.alpha = 0
+        secondView.alpha = 1
     }
     
-
     /*
-    // MARK: - Navigation
+     
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
