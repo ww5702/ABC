4b73bf04bcfa311061d843668cb26514e7d4b077
ios <wow5702@naver.com>
2023-03-29 15:50:25 +0900

    RootViewController 와 ContainerViewController간의 score 전달

--- ABC/VisualMemoryFirstViewController.swift
+++ ABC/VisualMemoryFirstViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
+// 대리자 프로토콜 작성
+
 protocol ContainerVCDelegate: AnyObject {
-    func didReceivedValueFromContainer(_ controller: VisualMemoryFirstViewController, value: String)
+    // 자식 VC로부터 값을 전달받은뒤 부모 컨테이너에서 그 값을 다루는 내용을 구현
+    func didReceivedValueFromContainer(_ controller: VisualMemoryFirstViewController, value: Int)
 }
 
 class VisualMemoryFirstViewController: UIViewController {
@@ -23,11 +26,13 @@ class VisualMemoryFirstViewController: UIViewController {
     @IBOutlet weak var btn8Label: UIButton!
     @IBOutlet weak var btn9Label: UIButton!
     
+    // delegate변수 설정, ContainerVCDelegate를 준수한다면 어느 클래스나 구조체도 여기에 할당가능
     weak var delegate: ContainerVCDelegate?
     
     let WHITE = UIColor.white
     let BLUE = UIColor.systemBlue
-    // 여기에 값 가져오기
+    // 부모VC로부터 받은 값을 savescore에 전달(lblReceivedValueFromRoot)
+    // 그리고 savescore를 다시 부모VC로 전달해줄것이다
     var savescore = 0
 
     override func viewDidLoad() {
@@ -35,14 +40,16 @@ class VisualMemoryFirstViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // 게임시작
     }
     
 
     @IBAction func btn1(_ sender: UIButton) {
         btn1Label.backgroundColor = WHITE
         print("1번 눌림")
+        // 일단 점수를 1점 늘린다음 스코어를 전달
         savescore += 1
-        delegate?.didReceivedValueFromContainer(self, value: "\(savescore)")
+        delegate?.didReceivedValueFromContainer(self, value: savescore)
         
     }
     @IBAction func btn2(_ sender: UIButton) {
@@ -64,6 +71,9 @@ class VisualMemoryFirstViewController: UIViewController {
         print("9번 눌림")
     }
     
+    func gameStart() {
+    }
+    
     func setblue() {
         btn1Label.backgroundColor = BLUE
         btn2Label.backgroundColor = BLUE
@@ -74,6 +84,10 @@ class VisualMemoryFirstViewController: UIViewController {
         btn7Label.backgroundColor = BLUE
         btn8Label.backgroundColor = BLUE
         btn9Label.backgroundColor = BLUE
+    }
+    
+    func setScore(_ value: Int) {
+        savescore = value
     }
 
 }
--- ABC/VisualMemoryViewController.swift
+++ ABC/VisualMemoryViewController.swift
@@ -11,9 +11,12 @@ class VisualMemoryViewController: UIViewController {
     
     @IBOutlet weak var firstView: UIView!
     @IBOutlet weak var secondView: UIView!
+    // 컨테이너 뷰 컨트롤러를 참조하는 변수 생성
+    // 해당 변수가 있어야 VisualMemoryViewController의 아무곳에서 나중에 보낼 타깃을 가리킬수있다.
     private var containerVC: VisualMemoryFirstViewController?
     
     @IBOutlet weak var lifeLabel: UILabel!
+    // 자식VC로 부터 받은 값을 표시할 레이블
     @IBOutlet weak var scoreLabel: UILabel!
     
     let WHITE = UIColor.white
@@ -36,12 +39,19 @@ class VisualMemoryViewController: UIViewController {
     @IBAction func gameStart(_ sender: Any) {
         firstView.alpha = 1
         secondView.alpha = 0
+        // 스코어를 전송
+        containerVC?.setScore(score)
     }
     
+    // segue.identifer별 분기 설정
+    // 해당 함수는 뷰 컨트롤러의 라이프 사이클의 초기에 실행된다.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case "firstContainer":
+            // 컨테이너 뷰 컨트롤러를 containerVC에 지정
             containerVC = segue.destination as? VisualMemoryFirstViewController
+            
+            // containerVC의 delegate변수를 RootVC 자신(self)로 지정
             containerVC?.delegate = self
             
         default:
@@ -52,8 +62,13 @@ class VisualMemoryViewController: UIViewController {
 
 }
 
+// ContainerVCDelegate를 준수하는 확장 구현
 extension VisualMemoryViewController: ContainerVCDelegate {
-    func didReceivedValueFromContainer(_ controller: VisualMemoryFirstViewController, value: String) {
+    func didReceivedValueFromContainer(_ controller: VisualMemoryFirstViewController, value: Int) {
         scoreLabel.text = "Score | \(value)"
+        if value >= 3 {
+            firstView.alpha = 0
+            secondView.alpha = 1
+        }
     }
 }
