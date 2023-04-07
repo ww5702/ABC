bc98ab17a81832d9a9c3c0aece005ea5b05466b7
ios <wow5702@naver.com>
2023-04-07 21:10:33 +0900

    NumberMermoy 정답 맞췄을시 페이지 구성

--- ABC/NumberMemory/NumberMemoryRightViewController.swift
+++ ABC/NumberMemory/NumberMemoryRightViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class NumberMemoryRightViewController: UIViewController {
-
     
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var myAnswerLabel: UILabel!
     @IBOutlet weak var levelLabel: UILabel!
     
+    var answer: Int = 0
+    var myAnswer: Int = 0
+    var level: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,21 @@ class NumberMemoryRightViewController: UIViewController {
     
     @IBAction func nextBtn(_ sender: UIButton) {
         
+    }
+    
+    func setAnswer(_ value: Int) {
+        answer = value
+    }
+    func setMyAnswer(_ value: Int) {
+        myAnswer = value
+    }
+    func setLevel(_ value: Int) {
+        level = value
+    }
+    func play() {
+        answerLabel.text = String(answer)
+        myAnswerLabel.text = String(myAnswer)
+        levelLabel.text = "\(level) LEVEL COMPLETE"
     }
     
 }
--- ABC/NumberMemory/NumberMemoryViewController.swift
+++ ABC/NumberMemory/NumberMemoryViewController.swift
@@ -11,20 +11,26 @@ class NumberMemoryViewController: UIViewController {
 
     @IBOutlet weak var firstView: UIView!
     @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    @IBOutlet weak var fourthView: UIView!
     
     var timeEnd: Bool = false
     private var numbercontainerVC: NumberMemoryFirstViewController?
     private var numbercontainerVC2: NumberMemorySecondViewController?
+    private var numbercontainerVC3: NumberMemoryRightViewController?
     
     var answer: Int = 0
     var myAnswer: Int = 0
     var iscorrectTime = false
+    var level: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         firstView.alpha = 1
         secondView.alpha = 0
+        thirdView.alpha = 0
+        fourthView.alpha = 0
         
         // Do any additional setup after loading the view.
     }
@@ -38,6 +44,9 @@ class NumberMemoryViewController: UIViewController {
         case "secondContainer":
             numbercontainerVC2 = segue.destination as? NumberMemorySecondViewController
             numbercontainerVC2?.delegate = self
+            
+        case "thirdContainer":
+            numbercontainerVC3 = segue.destination as? NumberMemoryRightViewController
 
         default:
             break
@@ -57,13 +66,15 @@ extension NumberMemoryViewController: ContainerVCDelegatenumber {
     }
     func didReceivedValueFromContainerNum(_ controller: NumberMemoryFirstViewController, value: Int) {
         answer = value
-        print("asdf\(answer)")
+        print("정답지 = \(answer)")
         firstView.alpha = 0
         secondView.alpha = 1
+        thirdView.alpha = 0
+        fourthView.alpha = 0
         iscorrectTime = true
         numbercontainerVC2?.setAnswer(answer)
         numbercontainerVC2?.iscorrectTime(iscorrectTime)
-        numbercontainerVC2?.setgamego()
+        //numbercontainerVC2?.setgamego()
     }
 }
 
@@ -71,5 +82,21 @@ extension NumberMemoryViewController: ContainerVCDelegatenumber2 {
     func didReceivedValueFromContainerNum(_ controller: NumberMemorySecondViewController, value: Int) {
         myAnswer = value
         print("받아온정답 = \(myAnswer)")
+        if myAnswer == answer {
+            level += 1
+            firstView.alpha = 0
+            secondView.alpha = 0
+            thirdView.alpha = 1
+            fourthView.alpha = 0
+            numbercontainerVC3?.setAnswer(answer)
+            numbercontainerVC3?.setMyAnswer(myAnswer)
+            numbercontainerVC3?.setLevel(level)
+            numbercontainerVC3?.play()
+        } else {
+            firstView.alpha = 0
+            secondView.alpha = 0
+            thirdView.alpha = 0
+            fourthView.alpha = 1
+        }
     }
 }
