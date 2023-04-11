d2213f117dfd9d4736926b02a8e973c18f8b248a
ios <wow5702@naver.com>
2023-04-11 17:27:18 +0900

    AimTrainer 버튼 터치마다 count -1

--- ABC/AimTrainer/AimTrainerViewController.swift
+++ ABC/AimTrainer/AimTrainerViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 class AimTrainerViewController: UIViewController {
     
     
-    @IBOutlet weak var btn1Label: UIButton!
-    @IBOutlet weak var testbtnLabel: UIButton!
+    @IBOutlet weak var startLabel: UIButton!
+    @IBOutlet weak var touchbtnLabel: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    
     
     var leftConstraint: NSLayoutConstraint?
     var trailingConstraint: NSLayoutConstraint?
@@ -20,54 +22,54 @@ class AimTrainerViewController: UIViewController {
     
     var randomup : Int = 0
     var randomleft : Int = 0
+    var count = 30
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        countLabel.text = "Reamining \(count)"
+        touchbtnLabel.layer.isHidden = true
     }
     
-    @IBAction func btn1(_ sender: UIButton) {
+    @IBAction func startbtn(_ sender: UIButton) {
+        touchbtnLabel.layer.isHidden = false
+        
         let safeArea = view.safeAreaLayoutGuide
         
-        randomup = Int.random(in: 0...700)
-        randomleft = Int.random(in: 0...350)
+        randomup = Int.random(in: 100...700)
+        randomleft = Int.random(in: 0...300)
         print(randomup,randomleft)
         
-        testbtnLabel.backgroundColor = UIColor.orange
-        testbtnLabel.translatesAutoresizingMaskIntoConstraints = false
+        touchbtnLabel.backgroundColor = UIColor.orange
+        touchbtnLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        topConstraint = testbtnLabel.topAnchor.constraint(equalTo: safeArea.topAnchor,constant: CGFloat(randomup))
+        topConstraint = touchbtnLabel.topAnchor.constraint(equalTo: safeArea.topAnchor,constant: CGFloat(randomup))
         topConstraint?.isActive = true
         
-        leftConstraint = testbtnLabel.leftAnchor.constraint(equalTo: safeArea.leftAnchor,constant: CGFloat(randomleft))
+        leftConstraint = touchbtnLabel.leftAnchor.constraint(equalTo: safeArea.leftAnchor,constant: CGFloat(randomleft))
         leftConstraint?.isActive = true
         
-//        trailingConstraint = testbtnLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-//        trailingConstraint?.isActive = true
-        
-
-//        bottomConstraint = testbtnLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-//        bottomConstraint?.isActive = true
-        
         // 시작 후 버튼 숨기기
-        btn1Label.layer.isHidden = true
+        startLabel.layer.isHidden = true
         
     }
     
     
-    @IBAction func testBtn(_ sender: UIButton) {
+    @IBAction func touchBtn(_ sender: UIButton) {
         
         let safeArea = view.safeAreaLayoutGuide
         
-        randomup = Int.random(in: 0...700)
-        randomleft = Int.random(in: 0...350)
+        randomup = Int.random(in: 100...700)
+        randomleft = Int.random(in: 0...300)
         print(randomup,randomleft)
+        
+        count -= 1
+        countLabel.text = "Reamining \(count)"
         
         leftConstraint?.isActive = false
         topConstraint?.isActive = false
         //<- 재정의 하기 위해서 isActive를 false로 바꿔주고 다시 32번 줄과 같이 true로 한다.
-        leftConstraint = testbtnLabel.leftAnchor.constraint(equalTo: safeArea.leftAnchor,constant: CGFloat(randomleft))
-        topConstraint = testbtnLabel.topAnchor.constraint(equalTo: safeArea.topAnchor,constant: CGFloat(randomup))
+        leftConstraint = touchbtnLabel.leftAnchor.constraint(equalTo: safeArea.leftAnchor,constant: CGFloat(randomleft))
+        topConstraint = touchbtnLabel.topAnchor.constraint(equalTo: safeArea.topAnchor,constant: CGFloat(randomup))
         
         leftConstraint?.isActive = true
         topConstraint?.isActive = true
