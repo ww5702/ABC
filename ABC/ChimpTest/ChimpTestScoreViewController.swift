8b9446b3e24e55b631a0a45d70176e2adbb98a14
ios <wow5702@naver.com>
2023-04-20 17:20:41 +0900

    ChimpTest Score 코드 간결화

--- ABC/ChimpTest/ChimpTestScoreViewController.swift
+++ ABC/ChimpTest/ChimpTestScoreViewController.swift
@@ -19,7 +19,7 @@ class ChimpTestScoreViewController: UIViewController {
     
     let RED = UIColor(named: "turnRed")
     
-    var data = 16
+    var data = 4
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,57 +29,44 @@ class ChimpTestScoreViewController: UIViewController {
 //        lottieView.play()
         
         if data < 5 {
-            animationView = .init(name: "bronze_trophy2")
-            animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-            animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 250)
-            animationView!.contentMode = .scaleAspectFit
-            animationView!.loopMode = .loop
-            animationView!.animationSpeed = 0.5
-            view.addSubview(animationView!)
-            animationView!.play()
-            
+            trophyAnimation(x: "bronze_trophy")
             scoreLabel.text = "\(data) 점"
-            explainLabel.text = "안타까워요..\n90%의 침팬지는 9자리를 기억할 수 있습니다.\n다시 도전해보세요"
+            textAnimation(x: "안타까워요..근데 그거 아시나요?\n90%의 침팬지는 9자리를 기억할 수 있습니다.\n아니 그냥 그렇다구요ㅎㅎ")
         } else if data <= 9 {
-            animationView = .init(name: "test2")
-            animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-            animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 250)
-            animationView!.contentMode = .scaleAspectFit
-            animationView!.loopMode = .loop
-            animationView!.animationSpeed = 0.5
-            view.addSubview(animationView!)
-            animationView!.play()
-            
+            trophyAnimation(x: "silver_trophy")
             scoreLabel.text = "\(data) 점"
-            explainLabel.text = "흐으음...\n90%의 침팬지는 9자리를 기억할 수 있습니다.\n침팬지 정도는 되시는군요!"
+            textAnimation(x: "흐으음...\n90%의 침팬지는 9자리를 기억할 수 있습니다.\n 침팬지 정도는 되시는군요!")
         } else if data <= 13 {
-            animationView = .init(name: "gold_trophy")
-            animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-            animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 250)
-            animationView!.contentMode = .scaleAspectFit
-            animationView!.loopMode = .loop
-            animationView!.animationSpeed = 0.5
-            view.addSubview(animationView!)
-            animationView!.play()
-            
+            trophyAnimation(x: "gold_trophy")
             scoreLabel.text = "\(data) 점"
-            explainLabel.text = "GREAT!\n최고는 아니지만 이정도면 어디가서 꿀리진 않겠어요!"
+            textAnimation(x: "GREAT!\n최고는 아니지만 이정도면 어디가서 꿀리진 않겠어요!")
         } else {
-            animationView = .init(name: "gold_trophy2")
-            animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-            animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 250)
-            animationView!.contentMode = .scaleAspectFit
-            animationView!.loopMode = .loop
-            animationView!.animationSpeed = 0.5
-            view.addSubview(animationView!)
-            animationView!.play()
-            
+            trophyAnimation(x: "champion_trophy")
             scoreLabel.text = "\(data) 점"
-            explainLabel.text = "!!CHAMPION!!\n이보다 좋은 결과는 없을거에요"
+            textAnimation(x: "!!CHAMPION!!\n이보다 좋은 결과는 없을거에요")
         }
-        
-        
         changeTextColor()
+    }
+    func trophyAnimation(x : String) {
+        animationView = .init(name: x)
+        animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 250)
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.loopMode = .loop
+        animationView!.animationSpeed = 0.5
+        view.addSubview(animationView!)
+        animationView!.play()
+    }
+    func textAnimation(x : String) {
+        let explainText = x
+        explainLabel.text = ""
+        var charIndex = 0.0
+        for letter in explainText {
+            Timer.scheduledTimer(withTimeInterval: 0.05 * charIndex, repeats: false) {
+                Timer in self.explainLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
     func changeTextColor() {
         guard let text = self.scoreLabel.text else {return}
