d0ce7cfab1d2a4d5ede353b0fcf304ecd6f8d7f1
ios <wow5702@naver.com>
2023-04-26 17:27:33 +0900

    VerbalMemory Score VC 애니메이션 추가

--- ABC/VerbalMemory/VerbalMemoryScoreViewController.swift
+++ ABC/VerbalMemory/VerbalMemoryScoreViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 이재웅 on 2023/03/21.
 //
 
+import Lottie
 import UIKit
 
 class VerbalMemoryScoreViewController: UIViewController {
@@ -12,17 +13,54 @@ class VerbalMemoryScoreViewController: UIViewController {
     
     @IBOutlet weak var explainLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
-    var data = 0
+    private var animationView: LottieAnimationView?
+    var data = 60
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.scoreLabel.text = "\(data)점"
-        self.explainLabel.text = "인간은 평균적으로 \n30점을 기록합니다."
-        
+        self.scoreLabel.text = "\(data) words"
         self.changeTextColor()
+        
+        if data < 20 {
+            trophyAnimation(x: "bronze_trophy")
+            scoreLabel.text = "\(data) 점"
+            textAnimation(x: "\(data)개..?\n그래요 영어단어라서 이렇게 못한거라고 위안하죠\n암 그렇고말고!")
+        } else if data <= 30 {
+            trophyAnimation(x: "silver_trophy")
+            scoreLabel.text = "\(data) 점"
+            textAnimation(x: "흐으음...\n대부분의 사람들은 30개 정도의 단어를 기억합니다.\n 즉 '평균'은 되신다는 소리죠!")
+        } else if data <= 60 {
+            trophyAnimation(x: "gold_trophy")
+            scoreLabel.text = "\(data) 점"
+            textAnimation(x: "GREAT!\n최고는 아니지만 이정도면 어디가서 꿀리진 않겠어요!")
+        } else {
+            trophyAnimation(x: "champion_trophy")
+            scoreLabel.text = "\(data) 점"
+            textAnimation(x: "!!CHAMPION!!\n믿기지 않는 기억력!\n이보다 좋은 결과는 없을거에요")
+        }
     }
-    
+    func trophyAnimation(x : String) {
+        animationView = .init(name: x)
+        animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 250)
+        animationView!.contentMode = .scaleAspectFit
+        animationView!.loopMode = .loop
+        animationView!.animationSpeed = 0.5
+        view.addSubview(animationView!)
+        animationView!.play()
+    }
+    func textAnimation(x : String) {
+        let explainText = x
+        explainLabel.text = ""
+        var charIndex = 0.0
+        for letter in explainText {
+            Timer.scheduledTimer(withTimeInterval: 0.05 * charIndex, repeats: false) {
+                Timer in self.explainLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
     func changeTextColor() {
         guard let text = self.scoreLabel.text else {return}
         let font = UIFont.systemFont(ofSize: 50)
