57402a96fa945c2aae24363ea7fb479510322c18
ios <wow5702@naver.com>
2023-05-02 16:40:43 +0900

    Rank VC에 TableView 추가

--- ABC/RankViewController.swift
+++ ABC/RankViewController.swift
@@ -7,12 +7,25 @@
 
 import UIKit
 
-class RankViewController: UIViewController {
+var name = ["1","2"]
+
+class RankViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = name[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
 
