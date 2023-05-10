e8cdfd24cb8055e0f73fe57a88e12406051e4d39
ios <wow5702@naver.com>
2023-05-10 16:06:45 +0900

    키보드 화면 가림 문제 해결

--- ABC/ViewController.swift
+++ ABC/ViewController.swift
@@ -29,13 +29,7 @@ class ViewController: UIViewController {
         
         toolbar.setItems([flexBarButton, doneButton], animated: false)
         nameTextField.inputAccessoryView = toolbar
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-        
 
-        // Do any additional setup after loading the view.
-        
         animationView = .init(name: "main")
         animationView!.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
         animationView!.center = CGPoint(x: self.view.frame.size.width/2, y: 300)
@@ -44,21 +38,19 @@ class ViewController: UIViewController {
         animationView!.animationSpeed = 0.5
         view.addSubview(animationView!)
         animationView!.play()
-        
-        // 키보드 내리기
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tap)
 
 
         //dbHelper.createTable()
         //dbHelper.insertData(name: "2번", value: 20, section: "aim")
         //dbHelper.updateDate(id: 1, name: "수정 테스터", age: 10)
     }
-    
-    // 키보드 내리기
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
+    override func viewWillAppear(_ animated: Bool) {
+        self.addKeyboardNotifications()
     }
+    override func viewWillDisappear(_ animated: Bool) {
+        self.removeKeyboardNotifications()
+    }
+    
     @objc func done() {
         self.view.endEditing(true)
     }
@@ -85,21 +77,42 @@ class ViewController: UIViewController {
             }
             
         }
-        
     }
-    @objc func keyboardWillShow(_ sender: Notification) {
-            let userInfo:NSDictionary = sender.userInfo! as NSDictionary
-            let keyboardFrame:NSValue = userInfo.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue
+    
+    // 키보드가 나타났다는 알림을 받으면 실행할 메서드
+    @objc func keyboardWillShow(_ noti: NSNotification){
+        // 키보드의 높이만큼 화면을 올려준다.
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardRectangle = keyboardFrame.cgRectValue
             let keyboardHeight = keyboardRectangle.height
-            keyHeight = keyboardHeight
+            self.view.frame.origin.y -= keyboardHeight
+        }
+    }
 
-            self.view.frame.size.height -= keyboardHeight
+    // 키보드가 사라졌다는 알림을 받으면 실행할 메서드
+    @objc func keyboardWillHide(_ noti: NSNotification){
+        // 키보드의 높이만큼 화면을 내려준다.
+        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            self.view.frame.origin.y += keyboardHeight
         }
-    @objc func keyboardWillHide(_ sender: Notification) {
-            
-            self.view.frame.size.height += keyHeight!
-        }
+    }
     
+    // 노티피케이션을 추가하는 메서드
+    func addKeyboardNotifications(){
+        // 키보드가 나타날 때 앱에게 알리는 메서드 추가
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification , object: nil)
+        // 키보드가 사라질 때 앱에게 알리는 메서드 추가
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    // 노티피케이션을 제거하는 메서드
+    func removeKeyboardNotifications(){
+        // 키보드가 나타날 때 앱에게 알리는 메서드 제거
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification , object: nil)
+        // 키보드가 사라질 때 앱에게 알리는 메서드 제거
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
 }
 
